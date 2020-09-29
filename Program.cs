912773a12b5f8cb996b9e6d698f33adb8627dcb2
Ruthikreddy <ruthikreddy15@gmail.com>
2020-09-29 23:08:12 +0530

    First commit of game

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeandLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("welcome to snake and ladder");
+            const int Start_Position = 0;
+        }
+    }
+}
