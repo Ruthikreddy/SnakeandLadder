5ddd580b6ed6cf104c2a3e8972af36cc5227cb3d
Ruthikreddy <ruthikreddy15@gmail.com>
2020-09-29 23:32:12 +0530

    commit for 3rd case

--- Program.cs
+++ Program.cs
@@ -8,10 +8,26 @@ namespace SnakeandLadder
         {
 
             Console.WriteLine("welcome to snake and ladder");
-            const int Start_Position = 0;
+            int NUMBER_OF_PLAYERS = 1;
+            int START_POSITION = 0;
+            int presentPosition = 0;
+            
             Random random = new Random();
             int numberOnDie = random.Next(1, 7);
-            Console.WriteLine("Die Number is {0}", numberOnDie);
+            int CheckOption = random.Next(0, 3);
+            switch (CheckOption)
+            {
+                case 0:
+                    break;
+                case 1:
+                    presentPosition += numberOnDie;
+                    break;
+                case 2:
+                    presentPosition -= numberOnDie;
+                    break;
+
+            }
+            Console.WriteLine(presentPosition);
         }
     }
 }
