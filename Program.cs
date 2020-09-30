669d261e9584dd4bac9c77a82209a5b75cd82d60
Ruthikreddy <ruthikreddy15@gmail.com>
2020-09-30 20:59:42 +0530

    Push for UC6

--- Program.cs
+++ Program.cs
@@ -1,42 +1,46 @@
 ﻿using System;
 
-namespace SnakeandLadder
+namespace SnakeAndLadder
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("welcome to snake and ladder");
             int NUMBER_OF_PLAYERS = 1;
             int START_POSITION = 0;
             int presentPosition = 0;
-            
+            const int NO_PLAY = 0;
+            const int LADDER = 1;
+            const int SNAKE = 2;
+            int totalNumberOfChances = 0;
+            int presentChanceNumber = 0;
             Random random = new Random();
-            while (presentPosition <100)
+            while (presentPosition < 100)
             {
+                presentChanceNumber++;
                 int numberOnDie = random.Next(1, 7);
-                int CheckOption = random.Next(0, 3);
-                switch (CheckOption)
+                int optionNumber = random.Next(0, 3);
+                switch (optionNumber)
                 {
-                    case 0:
+                    case NO_PLAY:
                         break;
-                    case 1:
-                        presentPosition += numberOnDie;
+                    case LADDER:
                         if (presentPosition + numberOnDie <= 100)
                             presentPosition += numberOnDie;
                         break;
-                    case 2:
+                    case SNAKE:
                         presentPosition -= numberOnDie;
+                        if (presentPosition < 0)
+                            presentPosition = 0;
                         break;
-                        
 
                 }
-                if (presentPosition <= 0)
-                    presentPosition = 0;
-
+                Console.WriteLine("The position after die roll number {0} is {1}", presentChanceNumber, presentPosition);
+                totalNumberOfChances += 1;
             }
-            Console.WriteLine(presentPosition);
+            Console.WriteLine("Total number of chances taken to reach 100 is {0}", totalNumberOfChances);
+
         }
     }
 }
